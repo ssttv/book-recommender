c9b8c6c10a3d41007cca269e1469a26491a6ac21
Sergey Titov <2629230@gmail.com>
2019-12-30 19:49:26 +0300

    fix: change message limit

--- stomp_test_sender.py
+++ stomp_test_sender.py
@@ -1,4 +1,5 @@
 import stomp
+import time
 
 host_and_ports = [('0.0.0.0', 61613)]
 conn = stomp.Connection(host_and_ports=host_and_ports)
@@ -6,12 +7,14 @@ conn = stomp.Connection(host_and_ports=host_and_ports)
 conn.start()
 conn.connect('admin', 'password', wait=True)
 counter = 0
-while counter < 10:
-    test_msg = "BookmarkMessage{status=status, userId=userId, element=element, rate=rate, vol=vol, num=num, page=page, comment='comment'}"
+
+while counter <= 5:
+    test_msg = "BookmarkMessage" + str(counter) + "{status=status, userId=userId, element=element, rate=rate, vol=vol, num=num, page=page, comment='comment'}"
     conn.send(body=test_msg, destination='/queue/messages')
     counter += 1
+    time.sleep(0.1)
 
-# time.sleep(1)
+time.sleep(1)
 # messages = base_listener.message_list
 # print(messages)
 conn.disconnect()
